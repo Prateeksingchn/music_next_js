e1252612f2313c8b0e558af5f7a855ce516a9be0
Prateeksingchn <prateeksinghchouhan4499@gmail.com>
2024-08-01 21:01:54 +0530

    next-js basic intro

---
+++ src/app/chai/lemon/page.tsx
@@ -0,0 +1,5 @@
+export default function LemonPage() {
+    return(
+        <h1 className="text-2xl">Lemon</h1>
+    )
+}
---
+++ src/app/chai/page.tsx
@@ -0,0 +1,5 @@
+export default function ChaiPage() {
+    return(
+        <h1 className="text-2xl">Chai</h1>
+    )
+}
